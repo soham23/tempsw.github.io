aff324738c03fdd6c44deef1c5b9d5e0fc500177
unknown <championsoham@gmail.com>
2019-08-21 07:04:23 +0530

    Commit v2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Check if Service Workers are supported
 if ('serviceWorker' in navigator){
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('../sw_cached_site.js')
+        navigator.serviceWorker.register('sw_cached_site.js')
         .then(reg => console.log('Service Worker : Registered'))
         .catch(err => console.log(`Service Worker: Error ${err}`))
     })
